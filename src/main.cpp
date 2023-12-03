2c1d60e11243ac8ee29e686c38e77289eadfa6e1
Mihai19S <mihaistinga19@gmail.com>
2023-12-02 21:37:52 -0500

    Affichage graphique

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,13 @@
 #include "TextDisplay.h"
 #include "Arial12x12.h"
 #include "Arial24x23.h"
-#include "touch_tft.h"
-#include "SPI_TFT.h"
 
 // the TFT is connected to SPI pin 5-7 and IO's 8-10
  SPI_TFT_ILI9341 TFT(D11, D12, D13, D10, D8, D9, "TFT"); // mosi, miso, sclk, cs, reset, dc
 // If your display need a signal for switch the backlight use a aditional IO pin in your program
 
-//touch_tft tt(A2, A4, A1, A3, D11, D12, D13, D10, D8, "TFT"); // x+,x-,y+,y-,mosi, miso, sclk, cs, reset
-
 int main()
 {
-
-    /*point p;
-
-    tt.claim(stdout);                         // send stdout to the TFT display
-    tt.background(Black);                     // set background to black
-    tt.foreground(White);                     // set chars to white
-    tt.cls();                                 // clear the screen
-    tt.set_font((unsigned char *)Arial12x12); // select the font
-    tt.set_orientation(3 );
-
-    tt.calibrate(); // calibrate the touch
-    while (1)
-    {
-        p = tt.get_touch(); // read analog pos.
-        if (tt.is_touched(p))
-        {                                     // test if touched
-            p = tt.to_pixel(p);               // convert to pixel pos
-            tt.fillcircle(p.x, p.y, 3, Blue); // print a blue dot on the screen
-        }
-    }*/
 
     TFT.claim(stdout); // send stderr to the TFT display
     TFT.background(Black); // set background to black
