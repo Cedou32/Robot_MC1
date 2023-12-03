0cd23c256d0e93df199bbe0886a92aace7152141
Cedou32 <frenettecedric8@gmail.com>
2023-12-03 12:21:43 -0500

    Initialisation lecture joystick

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include "mbed.h"
+#include "stdio.h"
 
 DigitalOut dir(D11);
 DigitalOut step(D10);
+AnalogIn Y(A0);
+AnalogIn X(A1);
+
+uint16_t valeur_x = 0;
 
 int main (){
     while(1){
+        uint16_t W = Y.read_u16();
+        printf("\n\r%d",W);
+        thread_sleep_for(1000);
+        /*
+        valeur_x = Y.read_u16();
+        printf("\n\r%d",valeur_x);
+        thread_sleep_for(1);*/
+        /*
+        // Code stepper fonctionnel
         dir = 1;
         for (uint8_t i = 0; i <= 200; i++){
             step = 1;
@@ -20,6 +34,6 @@ int main (){
             step = 0;
             thread_sleep_for(1);
         }
-        thread_sleep_for(250);
+        thread_sleep_for(250);*/
     }
 }
