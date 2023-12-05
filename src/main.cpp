2af9e2810cd6fc35bec8babbf0355aa94e68420b
Cedou32 <fenettecedric8@gmail.com>
2023-12-05 12:41:26 -0500

    Ajout du joystick commande stepper

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,25 @@ DigitalOut step(D10);
 AnalogIn Y(A0);
 AnalogIn X(A1);
 
-uint16_t valeur_x = 0;
+uint16_t valeur_y = 0;
 
 int main (){
     while(1){
-        uint16_t W = Y.read_u16();
-        printf("\n\r%d",W);
-        thread_sleep_for(1000);
-        /*
-        valeur_x = Y.read_u16();
-        printf("\n\r%d",valeur_x);
-        thread_sleep_for(1);*/
-        /*
-        // Code stepper fonctionnel
-        dir = 1;
-        for (uint8_t i = 0; i <= 200; i++){
-            step = 1;
-            thread_sleep_for(1);
-            step = 0;
-            thread_sleep_for(1);
+        
+        valeur_y = Y.read_u16();
+
+        while(valeur_y >= 31000 & valeur_y <= 34000){
+            valeur_y = Y.read_u16();
         }
-        thread_sleep_for(250);
-        dir = 0;
-        for (uint8_t i = 0; i <= 200; i++){
-            step = 1;
-            thread_sleep_for(1);
-            step = 0;
-            thread_sleep_for(1);
+        if(valeur_y > 33500){
+            dir = 1;
+        }else if (valeur_y < 32000){
+            dir = 0;
         }
-        thread_sleep_for(250);*/
+
+        step = 1;
+        thread_sleep_for(1);
+        step = 0;
+        thread_sleep_for(1);
     }
 }
