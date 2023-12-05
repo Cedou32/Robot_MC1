c0adefd15bcca47a2230e30fbc2468bcc1f3d975
Cedou32 <fenettecedric8@gmail.com>
2023-12-05 13:10:04 -0500

    Controle stepper machine a etat

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,43 @@ AnalogIn X(A1);
 
 uint16_t valeur_y = 0;
 
+enum {attente_mouvement, dir_droite, dir_gauche, mouv_un_step};
+uint8_t etat = 0;
+
+
+
 int main (){
+    etat = attente_mouvement;
     while(1){
-        
+        /**/
         valeur_y = Y.read_u16();
 
-        while(valeur_y >= 31000 & valeur_y <= 34000){
-            valeur_y = Y.read_u16();
-        }
-        if(valeur_y > 33500){
-            dir = 1;
-        }else if (valeur_y < 32000){
-            dir = 0;
-        }
+        switch(etat){
+            case attente_mouvement:
+                if(valeur_y > 35000){
+                    etat = dir_droite;
+                }else if(valeur_y < 31000){
+                    etat = dir_gauche;
+                }
+            break;
 
-        step = 1;
-        thread_sleep_for(1);
-        step = 0;
-        thread_sleep_for(1);
+            case dir_droite:
+                dir = 1;
+                etat = mouv_un_step;
+            break;
+
+            case dir_gauche:
+                dir = 0;
+                etat = mouv_un_step;
+            break;
+
+            case mouv_un_step:
+                step = 1;
+                thread_sleep_for(1);
+                step = 0;
+                thread_sleep_for(1);
+                etat = attente_mouvement;
+            break;
+        }
     }
 }
