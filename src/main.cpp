6801f7ac2ddc9a6d5b1ada75307f8cf324e3e72c
Mihai19S <mihaistinga19>
2024-01-26 13:53:53 -0500

    code de depart

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include "mbed.h"
 
-//          LISTE DE COMMANDE FOR AT25M01           //
+//          LISTE DE COMMANDE FOR ADXL312         //
 /*
-    Write Enable            =   0x06    
-    Write Disable           =   0x04    **Default at power up 
-    Read Status Register    =   0x05    
-    Write Status Register   =   0x01    **Suivi des 8 bits du registre
-    Read Sequence           =   0x03    **Suivi de l'adresse de 24 bits 
-    Write sequence          =   0x02    **Suivi de l'adresse de 24 bits
-
 
 */
 SPI spi(D11, D12, D13); // mosi, miso, sck
@@ -24,27 +17,6 @@ int main()
 
         cs = 0;
         spi.write(0x06);    
-        cs = 1;
-
-        cs = 0;
-        spi.write(0x05);            
-        int x = spi.write(0x00);    
-        cs = 1;
-
-        /*cs = 0;
-        spi.write(0x02);
-        spi.write(0x00);
-        spi.write(0x0A);
-        spi.write(0xFF);
-        spi.write(0x10);
-        cs = 1;*/
-
-        cs = 0;
-        spi.write(0x03);
-        spi.write(0x00);
-        spi.write(0x0A);
-        spi.write(0xFF);
-        int y = spi.write(0x00);
         cs = 1;
 
         thread_sleep_for(1000);
