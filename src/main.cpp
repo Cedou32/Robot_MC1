a09828d14a2c785ea5136f468df4673e60c25053
Mihai19S <mihaistinga19@gmail.com>
2024-01-26 23:47:27 -0500

    Ajout interface modes

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,9 @@ uint16_t positionY; //valeur de l'axe Y du toucher
 DigitalIn bouton_servo(PC_9); //bouton pour simuler le controle de la pince
 
 
-bool flag_menu = false; //flag pour indiquer si le bouton "menu" a ete appuye
-bool flag_vitesse = false;  //flag pour indiquer si le bouton "menu" a ete appuye
+bool flag_menu = false; //flag pour indiquer si le bouton "Menu" a ete appuye
+bool flag_vitesse = false;  //flag pour indiquer si le bouton "Vitesse" a ete appuye
+bool flag_modes = false; //flag pour indiquer si le bouton "Modes" a ete appuye
 
 uint8_t vitesse_moteur; //vitesse des stepper qui sera transmise
 
@@ -34,6 +35,11 @@ enum
   detection_appui,
   menu,
   vitesse,
+  modes,
+  libre,
+  enregistrer,
+  etendu,
+  debogage,
   selection_vitesse1,
   selection_vitesse2,
   selection_vitesse3,
@@ -67,10 +73,10 @@ int main()
 
           TFT.rect(0, 0, 320, 240, Blue);
 
-          TFT.rect(10, 10, 110, 40, White);          
+          TFT.rect(210, 10, 310, 40, White);          
           TFT.set_font((unsigned char *)Arial24x23); 
           TFT.foreground(Blue);                      
-          TFT.locate(20, 15);
+          TFT.locate(220, 15);
           TFT.printf("Menu");
 
           etat = attente_appui;
@@ -118,15 +124,37 @@ int main()
           printf("%d,%d\r\n",positionX,positionY);  //afficher 
 
           //si on appuie sur le bouton "Menu"
-          if (positionX >= 10 && positionX <= 110 && positionY >= 10 && positionY <= 40 && flag_menu == false){
+          if (positionX >= 210 && positionX <= 310 && positionY >= 10 && positionY <= 40 && flag_menu == false){
             flag_menu = true;  //activer le flag du bouton
             etat = menu;
           }
           //si on appuie sur le bouton "Vitesse"
-          else if(positionX >= 10 && positionX <= 90 && positionY >= 50 && positionY <= 70 && flag_vitesse == true){
+          else if(positionX >= 10 && positionX <= 90 && positionY >= 50 && positionY <= 70 && flag_vitesse == false){
             flag_vitesse = true; //activer le flag du bouton
             etat = vitesse;
           }
+          //si on appuie sur le bouton "Modes"
+          else if(positionX >= 10 && positionX <= 90 && positionY >= 80 && positionY <= 100){
+            flag_modes = true;
+            etat = modes;
+          }
+          //si on appuie sur le bouton "Libre"
+          else if(positionX >= 120 && positionX <= 210 && positionY >= 80 && positionY <= 100 && flag_modes == true){
+            etat = libre;
+          }
+          //si on appuie sur le bouton "Enregistrer"
+          else if(positionX >= 220 && positionX <= 310 && positionY >= 80 && positionY <= 100 && flag_modes == true){
+            etat = enregistrer;
+          }
+          //si on appuie sur le bouton "Etendu"
+          else if(positionX >= 120 && positionX <= 210 && positionY >= 110 && positionY <= 130 && flag_modes == true){
+            etat = etendu;
+          }
+          //si on appuie sur le bouton "Debogage"
+          else if(positionX >= 220 && positionX <= 310 && positionY >= 110 && positionY <= 130 && flag_modes == true){
+            etat = debogage;
+          }
+          
           //si on appuie sur le bouton de la vitesse "1"
           else if(positionX >= 120 && positionX <= 140 && positionY >= 50 && positionY <= 70 && flag_vitesse == true){
             etat = selection_vitesse1;
@@ -136,7 +164,7 @@ int main()
             etat = selection_vitesse2;
           }
           //si on appuie sur le bouton de la vitesse "3"
-          else if(positionX >= 180 && positionX <= 200 && positionY >= 50 && positionY <= 70 && flag_vitesse == false){
+          else if(positionX >= 180 && positionX <= 200 && positionY >= 50 && positionY <= 70 && flag_vitesse == true){
             etat = selection_vitesse3;
           }
           //si on appuie sur le bouton "Fermer"
@@ -154,10 +182,10 @@ int main()
 
         case menu:
             //changer la couleur du bouton pour indiquer qu'il a ete appuye
-            TFT.rect(10, 10, 110, 40, DarkGrey);          
+            TFT.rect(210, 10, 310, 40, DarkGrey);          
             TFT.set_font((unsigned char *)Arial24x23); 
             TFT.foreground(DarkGrey);                      
-            TFT.locate(20, 15);
+            TFT.locate(220, 15);
             TFT.printf("Menu");
 
             //afficher le bouton "Vitesse"
@@ -166,6 +194,13 @@ int main()
             TFT.foreground(Green);                     
             TFT.locate(25, 55);
             TFT.printf("Vitesse");
+
+            //afficher le bouton "Modes"
+            TFT.rect(10, 80, 90, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Orange);                     
+            TFT.locate(25, 85);
+            TFT.printf("Modes");
 
             //afficher le bouton "Fermer"
             TFT.rect(240, 210, 310, 230, White);       
@@ -205,6 +240,187 @@ int main()
             TFT.foreground(Red);                       
             TFT.locate(187, 57);
             TFT.printf("3");
+
+            etat = attente_appui;
+          break;
+
+        case modes:
+
+          //Changer la couleur du bouton
+            TFT.rect(10, 80, 90, 100, DarkGrey);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(DarkGrey);                     
+            TFT.locate(25, 85);
+            TFT.printf("Modes");
+
+            TFT.rect(120, 80, 210, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 85);
+            TFT.printf("Libre");
+
+            TFT.rect(220, 80, 310, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 85);
+            TFT.printf("Enregistrer");
+
+            TFT.rect(120, 110, 210, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 115);
+            TFT.printf("Etendu");
+
+            TFT.rect(220, 110, 310, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 115);
+            TFT.printf("Debogage");
+            
+
+            etat = attente_appui;
+          break; 
+
+        case libre:
+
+            TFT.rect(120, 80, 210, 100, DarkGrey);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(DarkGrey);                     
+            TFT.locate(140, 85);
+            TFT.printf("Libre");
+
+            TFT.rect(220, 80, 310, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 85);
+            TFT.printf("Enregistrer");
+
+            TFT.rect(120, 110, 210, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 115);
+            TFT.printf("Etendu");
+
+            TFT.rect(220, 110, 310, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 115);
+            TFT.printf("Debogage");
+
+            //afficher le bouton "Choisir"
+            TFT.rect(240, 180, 310, 200, White);       
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(White);                     
+            TFT.locate(252, 185);
+            TFT.printf("Choisir");
+
+            etat = attente_appui;
+          break;
+
+        case enregistrer:
+
+            TFT.rect(120, 80, 210, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 85);
+            TFT.printf("Libre");
+
+            TFT.rect(220, 80, 310, 100, DarkGrey);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(DarkGrey);                     
+            TFT.locate(225, 85);
+            TFT.printf("Enregistrer");
+
+            TFT.rect(120, 110, 210, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 115);
+            TFT.printf("Etendu");
+
+            TFT.rect(220, 110, 310, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 115);
+            TFT.printf("Debogage");
+
+            //afficher le bouton "Choisir"
+            TFT.rect(240, 180, 310, 200, White);       
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(White);                     
+            TFT.locate(252, 185);
+            TFT.printf("Choisir");
+
+            etat =  attente_appui;
+          break;
+
+        case etendu:
+
+            TFT.rect(120, 80, 210, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 85);
+            TFT.printf("Libre");
+
+            TFT.rect(220, 80, 310, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 85);
+            TFT.printf("Enregistrer");
+
+            TFT.rect(120, 110, 210, 130, DarkGrey);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(DarkGrey);                     
+            TFT.locate(140, 115);
+            TFT.printf("Etendu");
+
+            TFT.rect(220, 110, 310, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 115);
+            TFT.printf("Debogage");
+
+            //afficher le bouton "Choisir"
+            TFT.rect(240, 180, 310, 200, White);       
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(White);                     
+            TFT.locate(252, 185);
+            TFT.printf("Choisir");
+
+            etat = attente_appui;
+          break;
+
+        case debogage:
+
+            TFT.rect(120, 80, 210, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 85);
+            TFT.printf("Libre");
+
+            TFT.rect(220, 80, 310, 100, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(225, 85);
+            TFT.printf("Enregistrer");
+
+            TFT.rect(120, 110, 210, 130, White);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(Green);                     
+            TFT.locate(140, 115);
+            TFT.printf("Etendu");
+
+            TFT.rect(220, 110, 310, 130, DarkGrey);           
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(DarkGrey);                     
+            TFT.locate(225, 115);
+            TFT.printf("Debogage");
+
+            //afficher le bouton "Choisir"
+            TFT.rect(240, 180, 310, 200, White);       
+            TFT.set_font((unsigned char *)Arial12x12); 
+            TFT.foreground(White);                     
+            TFT.locate(252, 185);
+            TFT.printf("Choisir");
 
             etat = attente_appui;
           break;
@@ -316,17 +532,24 @@ int main()
             TFT.fillrect(10,50,200,70,Black);
             TFT.fillrect(240, 210, 310, 230, Black); 
             TFT.fillrect(240, 180, 310, 200, Black);
+            TFT.fillrect(10, 80, 90, 100, Black);  
+            TFT.fillrect(120, 80, 210, 100, Black);
+            TFT.fillrect(220, 80, 310, 100, Black);
+            TFT.fillrect(220, 110, 310, 130, Black);
+            TFT.fillrect(120, 110, 210, 130, Black);
+            
 
             //remettre la couleur originale du bouton "Menu"
-            TFT.rect(10, 10, 110, 40, White);          
+            TFT.rect(210, 10, 310, 40, White);          
             TFT.set_font((unsigned char *)Arial24x23); 
             TFT.foreground(Blue);                      
-            TFT.locate(20, 15);
+            TFT.locate(220, 15);
             TFT.printf("Menu");
 
             //baisser les flags
             flag_menu = false;
             flag_vitesse = false;
+            flag_modes = false;
 
             etat = attente_appui;
           break;
@@ -340,17 +563,23 @@ int main()
             TFT.fillrect(10,50,200,70,Black);
             TFT.fillrect(240, 210, 310, 230, Black); 
             TFT.fillrect(240, 180, 310, 200, Black);
+            TFT.fillrect(10, 80, 90, 100, Black);  
+            TFT.fillrect(120, 80, 210, 100, Black);
+            TFT.fillrect(220, 80, 310, 100, Black);
+            TFT.fillrect(220, 110, 310, 130, Black);
+            TFT.fillrect(120, 110, 210, 130, Black);
 
             //remettre la couleur originale du bouton "Menu"
-            TFT.rect(10, 10, 110, 40, White);          
+            TFT.rect(210, 10, 310, 40, White);          
             TFT.set_font((unsigned char *)Arial24x23); 
             TFT.foreground(Blue);                      
-            TFT.locate(20, 15);
+            TFT.locate(220, 15);
             TFT.printf("Menu");
 
-            //baisser les flags
+            //baisser les
             flag_menu = false;
             flag_vitesse = false;
+            flag_modes = false;
 
             etat = attente_appui;
           break;
