0c9b7045d76715b3c32a4ac48d3950584d60b791
Mihai19S <mihaistinga19@gmail.com>
2024-02-11 11:53:41 -0500

    Premier commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,30 +10,6 @@ uint16_t valeur_x = 0;
 
 int main (){
     while(1){
-        uint16_t W = Y.read_u16();
-        printf("\n\r%d",W);
-        thread_sleep_for(1000);
-        /*
-        valeur_x = Y.read_u16();
-        printf("\n\r%d",valeur_x);
-        thread_sleep_for(1);*/
-        /*
-        // Code stepper fonctionnel
-        dir = 1;
-        for (uint8_t i = 0; i <= 200; i++){
-            step = 1;
-            thread_sleep_for(1);
-            step = 0;
-            thread_sleep_for(1);
-        }
-        thread_sleep_for(250);
-        dir = 0;
-        for (uint8_t i = 0; i <= 200; i++){
-            step = 1;
-            thread_sleep_for(1);
-            step = 0;
-            thread_sleep_for(1);
-        }
-        thread_sleep_for(250);*/
+      
     }
 }
