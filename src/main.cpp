081c95a6302732449bd11c3acdb1a714d499f67b
Mihai19S <mihaistinga19@gmail.com>
2024-02-11 21:15:23 -0500

    mise a jour du code

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,14 @@ uint8_t buff1[] = "AB";
 AnalogIn joystickY(PA_0);
 AnalogIn joystickX(PA_1);
 
-uint8_t valueX;
-uint8_t valueY;
+uint8_t valueX, saveX;
+uint8_t valueY, saveY;
 
-uint8_t map(uint16_t raw_value){
-    uint8_t value_8bit = uint8_t (raw_value * 0.00389105);
-    return value_8bit;
+uint8_t map(uint16_t raw_value)
+{
+  uint8_t value_8bit = uint8_t(raw_value * 0.00389105);
+  return value_8bit;
 }
-
-
 
 int main()
 {
@@ -27,21 +26,22 @@ int main()
 
   pc.set_baud(115200);
 
-    while (1){
+  while (1)
+  {
 
-        buff[3] = map(joystickX.read_u16());
-        buff[4] = map(joystickY.read_u16());
+    valueX = map(joystickX.read_u16());
+    valueY = map(joystickY.read_u16());
 
-        
-        //buff1[0] = valueX;
-        //buff1[1] = valueY;
+    buff[3] = valueX;
+    buff[4] = valueY;
 
-        //printf("X = %u\r\n", buff[3]);
-        //printf("Y = %u\r\n", buff[4]);
-
-        pc.write(buff, sizeof(buff));
-        //pc.write(buff1, sizeof(buff1));
-
-        //thread_sleep_for(500);
+    if ((valueX - saveX) > 5 || (saveX - valueX) > 5 || (valueY - saveY) > 5 || (saveY - valueY) > 5)
+    {
+      pc.write(buff, sizeof(buff));
+      saveX = valueX;
+      saveY = valueY;
     }
+
+    // thread_sleep_for(500);
+  }
 }
