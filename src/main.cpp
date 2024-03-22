d8289df40f06ccc800574e83221e41096b2ecdf7
Mihai19S <mihaistinga19@gmail.com>
2024-03-22 13:09:18 -0400

    Controle ServoMoteur

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,11 @@ DigitalOut stepPinB(PC_1);
 DigitalOut dirPinC(PB_9);
 DigitalOut stepPinC(PB_8);
 
+PwmOut ServoCoude(PC_8);
+
 Ticker InterruptionBT1;
 Ticker InterruptionBT2;
+Ticker InterruptionBT3;
 
 char startMarker[] = "#@+";
 char endMarker[] = "?%";
@@ -21,14 +24,21 @@ char currentChar;
 uint8_t flagBT = 0;
 int flag_protection = 0;
 
+float dutyCycle = 0.05;
+
 void VerifStepperBase()
 {
-  if (buffer[3] >= 85 && buffer[3] <=170){
+  if (buffer[3] >= 85 && buffer[3] <= 170)
+  {
     stepPinB = 0;
-  }else if(buffer[3] >= 0 && buffer[3] < 85){
+  }
+  else if (buffer[3] >= 0 && buffer[3] < 85)
+  {
     dirPinB = 0;
     stepPinB = !stepPinB;
-  }else if(buffer[3] >= 170 && buffer[3] <= 270){
+  }
+  else if (buffer[3] >= 170 && buffer[3] <= 270)
+  {
     dirPinB = 1;
     stepPinB = !stepPinB;
   }
@@ -36,24 +46,59 @@ void VerifStepperBase()
 
 void VerifStepperCoude()
 {
-  if (buffer[5] >= 85 && buffer[5] <=170){
+  if (buffer[5] >= 85 && buffer[5] <= 170)
+  {
     stepPinC = 0;
-  }else if(buffer[5] >= 0 && buffer[5] < 85){
+  }
+  else if (buffer[5] >= 0 && buffer[5] < 85)
+  {
     dirPinC = 0;
     stepPinC = !stepPinC;
-  }else if(buffer[5] >= 170 && buffer[5] <= 270){
+  }
+  else if (buffer[5] >= 170 && buffer[5] <= 270)
+  {
     dirPinC = 1;
     stepPinC = !stepPinC;
   }
 }
 
+void VerifServoCoude()
+{
+  if (buffer[5] >= 85 && buffer[5] <= 170)
+  {
+    //ServoCoude.suspend();
+  }
+  else if (buffer[5] >= 0 && buffer[5] < 85)
+  {
+    //ServoCoude.resume();
+    
+    dutyCycle -= 0.0001;
+  }
+  else if (buffer[5] >= 170 && buffer[5] <= 270)
+  {
+    //ServoCoude.resume();
+    dutyCycle += 0.0001;
+  }
+  if(dutyCycle < 0.05){
+    dutyCycle = 0.05;
+  } else if(dutyCycle > 0.1){
+    dutyCycle = 0.1;
+  }
+  ServoCoude.write(dutyCycle);
+  
+}
+
 int main()
 {
-  InterruptionBT1.attach(&VerifStepperBase, 0.002);
-  InterruptionBT2.attach(&VerifStepperCoude, 0.002);
+  //InterruptionBT1.attach(&VerifStepperBase, 0.002);
+  //InterruptionBT2.attach(&VerifStepperCoude, 0.002);
+  InterruptionBT3.attach(&VerifServoCoude, 0.002);
+
+  //ServoCoude.period_ms(1);
   pc.set_baud(115200);
   while (1)
   {
+
     if (pc.readable())
     {
       pc.read(&currentChar, 1);
