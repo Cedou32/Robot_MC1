8d7a2010b27828661996752d72dfb27a017017bc
Mihai19S <mihaistinga19@gmail.com>
2024-03-26 21:25:01 -0400

    Position Home dans machine a etat

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,9 @@ int flag_protection = 0;
 int flagStepper = 0;
 int flagServo = 0;
 
-float dutyCycleCoude = 0.100;     //Min = 0.03   Max = 0.125
-float dutyCyclePoignet = 0.125;   //Min = 0.025   Max = 0.115
-float dutyCyclePince = 0.125;     //Fermer = 0.025 =    Ouvrir = 0.125 = 
+float dutyCycleCoude = 0.100;   // Min = 0.03   Max = 0.125
+float dutyCyclePoignet = 0.125; // Min = 0.025   Max = 0.115
+float dutyCyclePince = 0.125;   // Fermer = 0.025 =    Ouvrir = 0.125 =
 
 enum etat
 {
@@ -62,195 +62,177 @@ int main()
   etat etat_actuel = depart;
   while (1)
   {
-
-    for (int i = 0; i < 750; i++)
-    {
-      stepPinC = 1;
-      thread_sleep_for(2);
-      stepPinC = 0;
-      thread_sleep_for(2);
-    }
-    //thread_sleep_for(1000);
-    ServoCoude.write(0.03);
-    thread_sleep_for(750);
-    ServoPoignet.write(0.09);
-    thread_sleep_for(500);
-    ServoPince.write(0.12);
-    thread_sleep_for(500);
-    ServoPince.write(0.08);
-    while (1);
-
-    /*switch (etat_actuel)
+    switch (etat_actuel)
     {
     case depart:
-      /*for (int i = 0; i < 650; i++)
+      for (int i = 0; i < 750; i++)
       {
         stepPinC = 1;
         thread_sleep_for(2);
         stepPinC = 0;
         thread_sleep_for(2);
       }
-
-      // thread_sleep_for(100);
-
-      // thread_sleep_for(100);
-      ServoCoude.write(dutyCycleCoude);
-      thread_sleep_for(1000);
-      ServoPoignet.write(dutyCyclePoignet);
+      // thread_sleep_for(1000);
+      ServoCoude.write(0.03);
+      thread_sleep_for(750);
+      ServoPoignet.write(0.09);
+      thread_sleep_for(500);
+      ServoPince.write(0.12);
+      thread_sleep_for(500);
+      ServoPince.write(0.08);
 
       etat_actuel = attente_trame;
       break;
     case attente_trame:
+
+      while (pc.readable() == 0)
+      {
+      }
+      etat_actuel = mouvement_moteur;
       break;
-      /*case attente_trame:
-        while(1);
-        while (pc.readable() == 0)
-        {}
-        etat_actuel = mouvement_moteur;
-        break;
-      case mouvement_moteur:
-        if (pc.readable())
-        {
-          pc.read(&currentChar, 1);
+    case mouvement_moteur:
+      if (pc.readable())
+      {
+        pc.read(&currentChar, 1);
 
-          if (currentChar == '#')
+        if (currentChar == '#')
+        {
+          buffer[0] = currentChar;
+          flag_protection++;
+        }
+        else if (currentChar == '@' && flag_protection == 1)
+        {
+          buffer[1] = currentChar;
+          flag_protection++;
+        }
+        else if (currentChar == '+' && flag_protection == 2)
+        {
+          buffer[2] = currentChar;
+          flag_protection++;
+          for (int i = 3; i <= 12; i++)
           {
-            buffer[0] = currentChar;
-            flag_protection++;
-          }
-          else if (currentChar == '@' && flag_protection == 1)
-          {
-            buffer[1] = currentChar;
-            flag_protection++;
-          }
-          else if (currentChar == '+' && flag_protection == 2)
-          {
-            buffer[2] = currentChar;
-            flag_protection++;
-            for (int i = 3; i <= 12; i++)
-            {
-              pc.read(&currentChar, 1);
-              buffer[i] = currentChar;
-            }
-          }
-          else if (currentChar == '?' && flag_protection == 3)
-          {
-            buffer[13] = currentChar;
-            flag_protection++;
-          }
-          else if (currentChar == '%' && flag_protection == 4)
-          {
-            buffer[14] = currentChar;
-            flag_protection = 0;
-            pc.write(buffer, 15);
-          }
-          else
-          {
-            memset(buffer, 0, 15);
+            pc.read(&currentChar, 1);
+            buffer[i] = currentChar;
           }
         }
-        if (flagStepper == 1)
+        else if (currentChar == '?' && flag_protection == 3)
         {
-          flagStepper = 0;
-          if (buffer[3] >= 85 && buffer[3] <= 170)
-          {
-            stepPinB = 0;
-          }
-          else if (buffer[3] >= 0 && buffer[3] < 85)
-          {
-            dirPinB = 0;
-            stepPinB = !stepPinB;
-          }
-          else if (buffer[3] >= 170 && buffer[3] <= 270)
-          {
-            dirPinB = 1;
-            stepPinB = !stepPinB;
-          }
-
-          if (buffer[6] >= 85 && buffer[6] <= 170)
-          {
-            stepPinC = 0;
-          }
-          else if (buffer[6] >= 0 && buffer[6] < 85)
-          {
-            dirPinC = 0;
-            stepPinC = !stepPinC;
-          }
-          else if (buffer[6] >= 170 && buffer[6] <= 270)
-          {
-            dirPinC = 1;
-            stepPinC = !stepPinC;
-          }
+          buffer[13] = currentChar;
+          flag_protection++;
+        }
+        else if (currentChar == '%' && flag_protection == 4)
+        {
+          buffer[14] = currentChar;
+          flag_protection = 0;
+          pc.write(buffer, 15);
+        }
+        else
+        {
+          memset(buffer, 0, 15);
+        }
+      }
+      if (flagStepper == 1)
+      {
+        flagStepper = 0;
+        if (buffer[3] >= 85 && buffer[3] <= 170)
+        {
+          stepPinB = 0;
+        }
+        else if (buffer[3] >= 0 && buffer[3] < 85)
+        {
+          dirPinB = 0;
+          stepPinB = !stepPinB;
+        }
+        else if (buffer[3] >= 170 && buffer[3] <= 270)
+        {
+          dirPinB = 1;
+          stepPinB = !stepPinB;
         }
 
-        if (flagServo == 1)
+        if (buffer[6] >= 85 && buffer[6] <= 170)
         {
-          flagServo = 0;
-          // Code du servo Poignet
-          if (buffer[5] >= 85 && buffer[5] <= 170)
-          {
-          }
-          else if (buffer[5] >= 0 && buffer[5] < 85)
-          {
-            dutyCycleCoude -= 0.00005;
-          }
-          else if (buffer[5] >= 170 && buffer[5] <= 255)
-          {
-            dutyCycleCoude += 0.00005;
-          }
-          if (dutyCycleCoude < 0.05)
-          {
-            dutyCycleCoude = 0.05;
-          }
-          else if (dutyCycleCoude > 0.125)
-          {
-            dutyCycleCoude = 0.125;
-          }
-          ServoCoude.write(dutyCycleCoude);
-          // Code du servo Coude
-          if (buffer[4] >= 85 && buffer[4] <= 170)
-          {
-          }
-          else if (buffer[4] >= 0 && buffer[4] < 85)
-          {
-            dutyCyclePoignet -= 0.00005;
-          }
-          else if (buffer[4] >= 170 && buffer[4] <= 255)
-          {
-            dutyCyclePoignet += 0.00005;
-          }
-          if (dutyCyclePoignet < 0.05)
-          {
-            dutyCyclePoignet = 0.05;
-          }
-          else if (dutyCyclePoignet > 0.125)
-          {
-            dutyCyclePoignet = 0.125;
-          }
-          ServoPoignet.write(dutyCyclePoignet);
-          // Code du servo Pince
-          if (buffer[7] >= 85 && buffer[7] <= 170)
-          {
-          }
-          else if (buffer[7] >= 0 && buffer[7] < 85)
-          {
-            dutyCyclePince -= 0.00005;
-          }
-          else if (buffer[7] >= 170 && buffer[7] <= 255)
-          {
-            dutyCyclePince += 0.00005;
-          }
-          if (dutyCyclePince < 0.05)
-          {
-            dutyCyclePince = 0.05;
-          }
-          else if (dutyCyclePince > 0.125)
-          {
-            dutyCyclePince = 0.125;
-          }
-          ServoPince.write(dutyCyclePince);
+          stepPinC = 0;
         }
-        break;*/
-    //}
+        else if (buffer[6] >= 0 && buffer[6] < 85)
+        {
+          dirPinC = 0;
+          stepPinC = !stepPinC;
+        }
+        else if (buffer[6] >= 170 && buffer[6] <= 270)
+        {
+          dirPinC = 1;
+          stepPinC = !stepPinC;
+        }
+      }
+
+      if (flagServo == 1)
+      {
+        flagServo = 0;
+        // Code du servo Poignet
+        if (buffer[5] >= 85 && buffer[5] <= 170)
+        {
+        }
+        else if (buffer[5] >= 0 && buffer[5] < 85)
+        {
+          dutyCycleCoude -= 0.00005;
+        }
+        else if (buffer[5] >= 170 && buffer[5] <= 255)
+        {
+          dutyCycleCoude += 0.00005;
+        }
+        if (dutyCycleCoude < 0.05)
+        {
+          dutyCycleCoude = 0.05;
+        }
+        else if (dutyCycleCoude > 0.125)
+        {
+          dutyCycleCoude = 0.125;
+        }
+        ServoCoude.write(dutyCycleCoude);
+        // Code du servo Coude
+        if (buffer[4] >= 85 && buffer[4] <= 170)
+        {
+        }
+        else if (buffer[4] >= 0 && buffer[4] < 85)
+        {
+          dutyCyclePoignet -= 0.00005;
+        }
+        else if (buffer[4] >= 170 && buffer[4] <= 255)
+        {
+          dutyCyclePoignet += 0.00005;
+        }
+        if (dutyCyclePoignet < 0.05)
+        {
+          dutyCyclePoignet = 0.05;
+        }
+        else if (dutyCyclePoignet > 0.125)
+        {
+          dutyCyclePoignet = 0.125;
+        }
+        ServoPoignet.write(dutyCyclePoignet);
+        // Code du servo Pince
+        if (buffer[7] >= 85 && buffer[7] <= 170)
+        {
+        }
+        else if (buffer[7] >= 0 && buffer[7] < 85)
+        {
+          dutyCyclePince -= 0.00005;
+        }
+        else if (buffer[7] >= 170 && buffer[7] <= 255)
+        {
+          dutyCyclePince += 0.00005;
+        }
+        if (dutyCyclePince < 0.05)
+        {
+          dutyCyclePince = 0.05;
+        }
+        else if (dutyCyclePince > 0.125)
+        {
+          dutyCyclePince = 0.125;
+        }
+        ServoPince.write(dutyCyclePince);
+      }
+      break;
+    }
   }
 }
