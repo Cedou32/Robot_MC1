f471d2f1c9dc96c62a768f1789b26ec7a55b20a5
Mihai19S <mihaistinga19@gmail.com>
2024-03-28 00:26:52 -0400

    Mode Libre

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "mbed.h"
 
 BufferedSerial pc(PB_6, PB_7); // USBTX et USBRX sont les broches de communication série sur la carte Nucleo STM32F072RB
-uint8_t buffer[15];
+uint8_t data[10];
+
 
 DigitalOut LED(PC_13);
 
@@ -50,15 +51,15 @@ void VerifStepper()
 
 int main()
 {
-  InterruptionServo.attach(&VerifServo, 0.002);
+  InterruptionServo.attach(&VerifServo, 0.001);
   InterruptionStepper.attach(&VerifStepper, 0.002);
 
-  // ServoCoude.period_ms(1);
-  buffer[3] = 100;
-  buffer[4] = 100;
-  buffer[5] = 100;
-  buffer[6] = 100;
-  buffer[7] = 100;
+  data[5] = 100;
+  data[6] = 100;
+  data[7] = 100;
+  data[8] = 100;
+  data[9] = 100;
+  
 
   pc.set_baud(115200);
   etat etat_actuel = depart;
@@ -101,70 +102,65 @@ int main()
         pc.read(&currentChar, 1);
         if (currentChar == '#')
         {
-          buffer[0] = currentChar;
           flag_protection++;
         }
         else if (currentChar == '@' && flag_protection == 1)
         {
-          buffer[1] = currentChar;
           flag_protection++;
         }
         else if (currentChar == '+' && flag_protection == 2)
         {
-          buffer[2] = currentChar;
           flag_protection++;
-          for (int i = 3; i <= 7; i++)
+          for (int i = 0; i <= 9; i++)
           {
             pc.read(&currentChar, 1);
-            buffer[i] = currentChar;
+            data[i] = currentChar;
           }
         }
         else if (currentChar == '?' && flag_protection == 3)
         {
-          buffer[8] = currentChar;
           flag_protection++;
         }
         else if (currentChar == '%' && flag_protection == 4)
         {
-          buffer[9] = currentChar;
           flag_protection = 0;
-          pc.write(buffer, 10);
+          pc.write(data, 10);
         }
         else
         {
-          memset(buffer, 0, 10);
+          memset(data, 100, 10);
         }
-        // LED = int(buffer[7]);
+        // LED = int(data[7]);
       }
 
       if (flagStepper == 1)
       {
         flagStepper = 0;
-        if (buffer[3] >= 85 && buffer[3] <= 170)
+        if (data[6] >= 85 && data[6] <= 170)
         {
           stepPinB = 0;
         }
-        else if (buffer[3] >= 0 && buffer[3] < 85)
+        else if (data[6] >= 0 && data[6] < 85)
         {
           dirPinB = 0;
           stepPinB = !stepPinB;
         }
-        else if (buffer[3] >= 170 && buffer[3] <= 270)
+        else if (data[6] >= 170 && data[6] <= 270)
         {
           dirPinB = 1;
           stepPinB = !stepPinB;
         }
 
-        if (buffer[6] >= 85 && buffer[6] <= 170)
+        if (data[9] >= 85 && data[9] <= 170)
         {
           stepPinC = 0;
         }
-        else if (buffer[6] >= 0 && buffer[6] < 85)
+        else if (data[9] >= 0 && data[9] < 85)
         {
           dirPinC = 0;
           stepPinC = !stepPinC;
         }
-        else if (buffer[6] >= 170 && buffer[6] <= 270)
+        else if (data[9] >= 170 && data[9] <= 270)
         {
           dirPinC = 1;
           stepPinC = !stepPinC;
@@ -175,14 +171,14 @@ int main()
       {
         flagServo = 0;
         // Code du servo Poignet
-        if (buffer[5] >= 85 && buffer[5] <= 170)
+        if (data[8] >= 85 && data[8] <= 170)
         {
         }
-        else if (buffer[5] >= 0 && buffer[5] < 85)
+        else if (data[85] >= 0 && data[8] < 85)
         {
           dutyCycleCoude -= 0.00005;
         }
-        else if (buffer[5] >= 170 && buffer[5] <= 255)
+        else if (data[8] >= 170 && data[8] <= 255)
         {
           dutyCycleCoude += 0.00005;
         }
@@ -196,14 +192,14 @@ int main()
         }
         ServoCoude.write(dutyCycleCoude);
         // Code du servo Coude
-        if (buffer[4] >= 85 && buffer[4] <= 170)
+        if (data[7] >= 85 && data[7] <= 170)
         {
         }
-        else if (buffer[4] >= 0 && buffer[4] < 85)
+        else if (data[7] >= 0 && data[7] < 85)
         {
           dutyCyclePoignet -= 0.00005;
         }
-        else if (buffer[4] >= 170 && buffer[4] <= 255)
+        else if (data[7] >= 170 && data[7] <= 255)
         {
           dutyCyclePoignet += 0.00005;
         }
@@ -217,14 +213,14 @@ int main()
         }
         ServoPoignet.write(dutyCyclePoignet);
         // Code du servo Pince
-        if (buffer[7] >= 85 && buffer[7] <= 170)
+        if (data[5] >= 85 && data[5] <= 170)
         {
         }
-        else if (buffer[7] >= 0 && buffer[7] < 85)
+        else if (data[5] >= 0 && data[5] < 85)
         {
           dutyCyclePince -= 0.00005;
         }
-        else if (buffer[7] >= 170 && buffer[7] <= 255)
+        else if (data[5] >= 170 && data[5] <= 255)
         {
           dutyCyclePince += 0.00005;
         }
